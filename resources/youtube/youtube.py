b0ac7cdf3a94da837a5f0b197c1e2617dd96d910
Hamza Shabbir <hamzashabbir447@gmail.com>
2024-03-16 22:26:57 +0500

    f

--- resources/youtube/youtube.py
+++ resources/youtube/youtube.py
@@ -1,19 +1,27 @@
 from pytube import YouTube
 import boto3
+import json
+import os
+
+
+YOUTUBE_BUCKET=os.environ['YOUTUBE_BUCKET']
+
+
 
 def handler(event,context):
-    url=event['pathParameters']['link']
+    print(event)
+    data=json.loads(event['body'])
+    url=data['url']
     video_url = url
     yt = YouTube(video_url)
     video_stream = yt.streams.get_highest_resolution()
-    download_path = 'hamza.mp4'
+    download_path = '/tmp/'
     video_stream.download(output_path=download_path)
     s3 = boto3.client('s3')
-    bucket_name = 'bilal-email-bucket'
-    file_key = 'hamza.mp4'
-    s3.upload_file(download_path, bucket_name, file_key)
-    return "File uploaded to S3"
-
-
+    bucket_name = YOUTUBE_BUCKET
+    for file in os.listdir(download_path):
+        if file.endswith('.mp4'):
+            s3.upload_file(download_path+file, bucket_name, file)
+            print('File uploaded to S3')
  
 
